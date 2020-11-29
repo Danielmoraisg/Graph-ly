586c3481e5455cd183ff1bf7e604899c50caa803
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2020-11-29 20:17:31 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -5,8 +5,11 @@ def app(state):
 	st.write('Click the button below to add your dataset and check if your data is correct')
 	upload = st.file_uploader('',accept_multiple_files = False)
 	if upload is not None:
-		state.data = pd.read_csv(upload)
 		state.name = upload.name
+		if state.name.split('.')[1] == 'csv':
+			state.data = pd.read_csv(upload)
+		elif state.name.split('.')[1] == 'xlsx':
+			state.data = pd.read_excel(upload)
 		st.write(state.data.head())
 		st.write(state.data.tail())
 	#st.write(df.data)
