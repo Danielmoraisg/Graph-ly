63b6bcb31c81f2ddfcb7af703e2feb0953a7c2e6
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2020-11-30 14:29:34 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -1,9 +1,22 @@
 import streamlit as st 
 import pandas as pd
+import os
+import base64
 def app(state):
 	st.title('Creating Plots')
 	st.write('Click the button below to add your dataset and check if your data is correct')
 	upload = st.file_uploader('',accept_multiple_files = False)
+
+	if st.checkbox('Download'):
+		def get_binary_file_downloader_html(bin_file, file_label='File'):
+			with open(bin_file, 'rb') as f:
+				data = f.read()
+			bin_str = base64.b64encode(data).decode()
+			href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+			return(href)
+		st.markdown(get_binary_file_downloader_html('template.xlsx', 'Excel template'), unsafe_allow_html=True)
+		st.markdown(get_binary_file_downloader_html('template.csv', 'Comma separated values template'), unsafe_allow_html=True)
+		st.markdown(get_binary_file_downloader_html('template.txt', 'Text file template'), unsafe_allow_html=True)
 	if upload is not None:
 		state.name = upload.name
 		if state.name.split('.')[1] == 'xlsx' or state.name.split('.')[1] == 'xls' :
