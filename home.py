7a13be7e4d7dd454e82f9e812fccf45b5d58413e
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2020-11-30 12:03:46 -0300

    Add files via upload
    solved a problem that causes excel files to generate am error once loaded

--- home.py
+++ home.py
@@ -6,12 +6,16 @@ def app(state):
 	upload = st.file_uploader('',accept_multiple_files = False)
 	if upload is not None:
 		state.name = upload.name
-		if state.name.split('.')[1] == 'csv':
-			state.data = pd.read_csv(upload)
+		if state.name.split('.')[1] == 'xlsx' or state.name.split('.')[1] == 'xls' :
+			#state.data = pd.read_excel(upload)
+			data = upload.read()
+			state.data = pd.read_excel(data)
+			#st.write(pd.read_excel(state.data))
 			st.write(state.data.head())
 			st.write(state.data.tail())
-		elif state.name.split('.')[1] == 'xlsx':
-			state.data = pd.read_excel(upload)
+
+		elif state.name.split('.')[1] == 'csv' :
+			state.data = pd.read_csv(upload)
 			st.write(state.data.head())
 			st.write(state.data.tail())
 		upload.seek(0)
