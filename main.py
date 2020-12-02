5af2cb52e0190d4e2b663b2e56d0fb12c5c31241
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2020-12-02 10:18:32 -0300

    Add files via upload
    added multi-bars plot page

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from States import _get_state
 import home
 import bar
 import line
+import multi_bar
 st.set_page_config(page_title="Graph-ly",page_icon=":fire:",layout="centered",initial_sidebar_state="auto")
 state = _get_state()
 
 PAGES = {
 	"Homepage":home,
-    "Grafico de linhas": line,
-    "Grafico de barras": bar
+    "Line Chart": line,
+    "Bar Chart": bar,
+    "Multiple Bars Chart":multi_bar
 }
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
