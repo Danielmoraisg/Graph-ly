73560da7b6cd7bb81f9c77636be10cfd32a2c6e5
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2020-12-02 15:58:53 -0300

    Update line.py

--- line.py
+++ line.py
@@ -38,7 +38,7 @@ def app(state):
     	img_bytes = Path(img_path).read_bytes()
     	encoded = base64.b64encode(img_bytes).decode()
     	return encoded
-    fig_html = "To download the plot click the camera icon <img src='data:image/png;base64,{}' width='18' height='15' class='img-fluid'>".format(img_to_bytes(r'images\download_icon.png'))
+    fig_html = "To download the plot click the camera icon <img src='data:image/png;base64,{}' width='18' height='15' class='img-fluid'>".format(img_to_bytes(r'images/download_icon.png'))
     st.markdown(fig_html, unsafe_allow_html=True)
 
 
