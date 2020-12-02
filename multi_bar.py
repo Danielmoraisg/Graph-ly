b019b926bce8d4d918de984502461b91182e434a
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2020-12-02 10:54:52 -0300

    Add files via upload

--- multi_bar.py
+++ multi_bar.py
@@ -12,11 +12,10 @@ def app(state):
     st.title('Bar Plot')
     Y = st.selectbox('Column to be mapped on the Y axis',state.data.columns)
     X = st.selectbox('Column to be mapped on the X axis',state.data.columns)
-    title = st.text_input("Title",state.name[0:-4])
     color = st.selectbox('Specify columns to differ bar color',state.data.columns)
     modes = {'Groups':'group', 'Stacked':'stack','Overlay':'overlay','Relative positioning':'relative'}
     mode = st.selectbox('How to display different groups', list(modes.keys()))
-    
+    title = st.text_input("Title",state.name[0:-4])    
     #advanced configs
     xtitle = X
     ytitle = Y
@@ -40,5 +39,5 @@ def app(state):
     	img_bytes = Path(img_path).read_bytes()
     	encoded = base64.b64encode(img_bytes).decode()
     	return encoded
-    fig_html = "To download the plot click the camera icon <img src='data:image/png;base64,{}' width='18' height='15' class='img-fluid'>".format(img_to_bytes(r'images/download_icon.png'))
+    fig_html = "To download the plot click the camera icon <img src='data:image/png;base64,{}' width='18' height='15' class='img-fluid'>".format(img_to_bytes(r'images\download_icon.png'))
     st.markdown(fig_html, unsafe_allow_html=True,)
